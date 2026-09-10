7081c9651377e598c2d82a8fc19850c09f1e5864
Megan <269440206+megansangster2001@users.noreply.github.com>
2026-09-10 12:03:20 +0100

    added fourth test

--- app/src/test/java/org/example/GildedRoseTest.java
+++ app/src/test/java/org/example/GildedRoseTest.java
@@ -8,7 +8,7 @@ class GildedRoseTest {
 
     @Test
     void normalItemQualityDecreases() {
-        Item[] items = new Item[] { new Item("foo", 10, 20) };
+        Item[] items = new Item[]{new Item("burger", 10, 20)};
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
@@ -18,7 +18,7 @@ class GildedRoseTest {
 
     @Test
     void qualityCannotGoBelowZero() {
-        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        Item[] items = new Item[]{new Item("steak", 10, 0)};
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
@@ -28,12 +28,22 @@ class GildedRoseTest {
 
     @Test
     void normalItemQualityDecreasesTwiceAsFastUseByDate() {
-        Item [] items = new Item[]{ new Item ("foo", 0, 10)};
+        Item[] items = new Item[]{new Item("chicken", 0, 10)};
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
 
         assertEquals(8, app.items[0].quality);
+    }
+    @Test
+    void agedBrieQualityIncreases() {
+        Item[] items = new Item [] { new Item("Aged Brie", 10, 20) };
+        GildedRose app = new GildedRose (items);
+
+        app.updateQuality();
+
+        assertEquals(21, app.items[0].quality);
+
     }
 
 }
