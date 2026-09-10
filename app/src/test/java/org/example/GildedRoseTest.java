fb3bff27ce0e29de836fe9a54748e0736ab22411
Megan <269440206+megansangster2001@users.noreply.github.com>
2026-09-10 11:52:40 +0100

    second etst

--- app/src/test/java/org/example/GildedRoseTest.java
+++ app/src/test/java/org/example/GildedRoseTest.java
@@ -8,12 +8,22 @@ class GildedRoseTest {
 
     @Test
     void normalItemQualityDecreases() {
-        Item[] items = new Item[] { new Item("brie", 10, 20) };
+        Item[] items = new Item[] { new Item("foo", 10, 20) };
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
 
         assertEquals(19, app.items[0].quality);
+    }
+
+    @Test
+    void qualityCannotGoBelowZero() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
     }
 
 }
