d1e9d9eb7db042feae70bd7bdadc387e805db027
Megan <269440206+megansangster2001@users.noreply.github.com>
2026-09-10 11:38:24 +0100

    first test passed for TDD

--- app/src/test/java/org/example/GildedRoseTest.java
+++ app/src/test/java/org/example/GildedRoseTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItemQualityDecreases() {
+        Item[] items = new Item[] { new Item("brie", 10, 20) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(19, app.items[0].quality);
     }
 
 }
--- app/src/test/java/org/example/TexttestFixture.java
+++ app/src/test/java/org/example/TexttestFixture.java
@@ -1,4 +1,4 @@
-package com.gildedrose;
+package org.example;
 
 public class TexttestFixture {
     public static void main(String[] args) {
